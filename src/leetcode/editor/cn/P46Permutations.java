3a9604d5b05216dec39fe4c5d158d09052c35665
cofire <yli_666@163.com>
2021-12-15 21:10:12 +0800

    46

--- src/leetcode/editor/cn/P46Permutations.java
+++ src/leetcode/editor/cn/P46Permutations.java
@@ -37,18 +37,52 @@
 
 
 package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //Java：全排列
-public class P46Permutations{
+public class P46Permutations {
     public static void main(String[] args) {
         Solution solution = new P46Permutations().new Solution();
+        System.out.println(solution.permute(new int[]{1, 2, 3, 4}));
         // TO TEST
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            backtracking(nums, 0, ans);
+            return ans;
+        }
+
+        public void backtracking(int[] nums, int level, List<List<Integer>> ans) {
+            if (level == nums.length - 1) {
+                ans.add(numsToList(nums));
+                return;
+            }
+            for (int i = level; i < nums.length; i++) {
+                swap(nums, i, level);
+                backtracking(nums, level + 1, ans);
+                swap(nums, i, level);
+            }
+        }
+
+        public List<Integer> numsToList(int[] nums) {
+            List<Integer> ls = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                ls.add(nums[i]);
+            }
+            return ls;
+        }
+
+        public void swap(int[] nums, int i, int j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
